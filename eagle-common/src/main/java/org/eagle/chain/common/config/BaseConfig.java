fc4a223ebcde4a6116500153d47da8aafac03bae
liyong <liam.li@qiyihuo.com>
2023-01-13 19:37:33 +0800

    fix pom,add jraft support

---
+++ eagle-common/src/main/java/org/eagle/chain/common/config/BaseConfig.java
@@ -0,0 +1,11 @@
+package org.eagle.chain.common.config;
+
+/**
+ * @author ApacheMoy
+ * @version create time：2023/1/13 19:14
+ * @describe:
+ */
+public class BaseConfig {
+    public static Integer P2P_PORT=8056;
+    
+}
--- eagle-consensus/src/main/java/org/eagle/chain/consensus/netty/serializer/Serializer.java
+++
@@ -1,7 +0,0 @@
-package org.eagle.chain.consensus.netty.serializer;
-
-public abstract class Serializer {
-    public abstract <T> byte[] serialize(T obj);
-
-    public abstract <T> Object deserialize(byte[] bytes, Class<T> clazz);
-}
